6c4d6f43ea8e08171649651d8c5f3b490d4d7096
Ivan Dario Posada <ivandarioposada@yahoo.com>
2023-05-16 18:27:07 -0500

    Seccion Idiomas
    Inicio de implementacion de maqueta seccion idiomas

--- idioma.php
+++ idioma.php
@@ -1,7 +1,30 @@
 <?php include('header.php') ?>
 <main>
-  <section class="">
+  <section class="row container nommbres">
+    <div class="col m8">
+      <div class="nombre-usuario">
+        Hola Laura
+      </div>
+      <div class="text-pequeno">
+        Inicia tu prueba
+      </div>
+    </div>
 
+    <div class="col m4 right">
+      <img src="img/navigation.png" alt="">
+    </div>
+  </section>
+
+  <section class="row container banner">
+    <img src="img/banner.png" alt="">
+  </section>
+
+  <section class="row container idioma">
+    <p>Elige un idioma para hacer tu prueba</p>
+
+    <div class="idioma-solo">
+      
+    </div>
   </section>
 </main>
 
