3cdd0293150c002711a317952ae0b9acae81495a
Ivan Dario Posada <ivandarioposada@yahoo.com>
2023-05-17 17:58:06 -0500

    Idiomas
    Finaluzacion de maquetas seccion idiomas

--- idioma.php
+++ idioma.php
@@ -22,9 +22,36 @@
   <section class="row container idioma">
     <p>Elige un idioma para hacer tu prueba</p>
 
-    <div class="idioma-solo">
-      
+    <div class="idioma-solo animate__animated animate__fadeInUp">
+      <img src="img/en-icon.png" alt="">
+      <div class="info-idioma">
+        <span class="nombre-idioma">Inglés</span>
+        <span class="num-preguntas">15 questions</span>
+      </div>
+
+      <a href="pregunta.php"> <img src="img/icon-flecha.png" alt=""> </a>
+
+
+
     </div>
+
+    <div class="idioma-solo animate__animated animate__fadeInUp">
+      <img src="img/fr-icon.png" alt="">
+      <div class="info-idioma">
+        <span class="nombre-idioma">Francés</span>
+        <span class="num-preguntas">15 questions</span>
+      </div>
+
+      <a href="pregunta.php"> <img src="img/icon-flecha.png" alt=""> </a>
+
+
+    </div>
+
+
+  </section>
+
+  <section class="img-abajo">
+    <img src="img/Banner-abajo.png" alt="">
   </section>
 </main>
 
