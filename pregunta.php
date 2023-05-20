3e7dc0aeff8cc35dbc769c511d8678fd95122471
Ivan Dario Posada <ivandarioposada@yahoo.com>
2023-05-20 10:50:58 -0500

    Pregunta
    FInalziacion de maqueta pregnta

---
+++ pregunta.php
@@ -0,0 +1,46 @@
+<?php include('header.php') ?>
+<section class="bg-morado">
+
+  <div class="container center num-pregunta">
+    1 / 10
+
+    <div class="timeline">
+      <img src="img/timeline.png" alt="">
+    </div>
+
+
+    <div class="frame-pregunta">
+
+      <h3 class="text-gris animate__animated animate__fadeIn">
+        Select an answer
+      </h3>
+
+      <h2 class="txt-pregunta animate__animated animate__fadeIn">
+        ¿What is this?
+      </h2>
+
+      <div class="img-pregunta animate__animated animate__zoomIn">
+        <img src="img/img-pregunta.png" alt="">
+      </div>
+
+      <div class="respuestas animate__animated animate__bounceIn">
+
+        <button class="btn waves-effect waves-light btn-respuesta" type="submit" name="action">
+          Submit
+        </button>
+        <button class="btn waves-effect waves-light btn-respuesta-correcta" type="submit" name="action">
+          Submit
+        </button>
+        <button class="btn waves-effect waves-light btn-respuesta" type="submit" name="action">
+          Submit
+        </button>
+
+        </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+<?php include('footer.php') ?>
