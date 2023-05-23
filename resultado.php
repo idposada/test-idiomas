027352d0cbfd55b42257bd08e7bf845d14c27545
Ivan Dario Posada <ivandarioposada@yahoo.com>
2023-05-23 12:47:21 -0500

    Resultado
    Finalzaicon d amqueta reslutado

---
+++ resultado.php
@@ -0,0 +1,48 @@
+<?php include('header.php') ?>
+
+<main>
+  <section class="row banner">
+    <img src="img/banner.png" alt="">
+  </section>
+
+  <section class="row container nommbres">
+
+      <div class="nombre-usuario">
+        Hola Laura
+    </div>
+    <div class="text-gris">
+      Aquí están tus resultados
+    </div>
+  </section>
+
+  <section class="row container resultado">
+    <img src="img/img-resultado.png" alt="">
+
+    <img src="img/img-indicador.png" alt="">
+
+    <div class="num-puntaje">
+      23
+  </div>
+
+  <div class="text-resultado">
+    ¡Sigue practicando!
+Tu nivel de inglés actual te sirve
+para mantener una charla
+con un niño de tres años.
+  </div>
+
+  <img src="img/img-abajo.png" alt="" style="width: 100%;">
+
+  </section>
+  <section class="row container center">
+    <div class="col m6 s6">
+      <img src="img/img-subir-nivel.png" alt="">
+    </div>
+
+    <div class="col m6 s6">
+      <img src="img/img-compartir.png" alt="">
+    </div>
+  </section>
+</main>
+
+<?php include('footer.php') ?>
